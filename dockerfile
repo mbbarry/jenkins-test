e7f75bb7ae24e76c1a101f3641d570ec8a251741
barry <mbbarry337@gmail.com>
2023-02-25 16:24:29 -0500

    Added dockerfile

---
+++ dockerfile
@@ -0,0 +1,8 @@
+FROM openjdk:11
+
+EXPOSE 8080
+
+COPY ./target/java-jenkins-build-1.0-SNAPSHOT.jar /user/app/
+WORKDIR /user/app
+
+ENTRYPOINT ["java", "-jar", "java-jenkins-build-1.0-SNAPSHOT.jar"]
--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import java.util.Calendar;
+import java.util.TimeZone;
+
 /**
  * Hello world!
  *
@@ -9,5 +12,11 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+        Calendar cal = Calendar.getInstance();
+        cal.setTimeZone(TimeZone.getTimeZone("America/Toronto"));
+        cal.setTimeInMillis(System.currentTimeMillis());
+
+        System.out.println(cal.get(Calendar.DAY_OF_WEEK));
     }
 }
